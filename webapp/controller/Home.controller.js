c8302820aa03886d9c1ca2ef8fee5dc803cbcabd
jhaeuss <juergen.haeussler@wieland.com>
2018-11-29 15:02:40 +0000

    Integration OAV_add - Step 1

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,9 +1,58 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox"
-], function (Controller, MessageBox) {
+	"sap/m/MessageBox",
+	"sap/ushell/services/Container"
+], function (Controller, MessageBox, Container) {
 	"use strict";
 	return Controller.extend("com.wieland.oav.controller.Home", {
+		onInit: function() {
+			if (this.isUserInGroup("Administrator")) {
+				var tile = this.getView().byId("NeuregistrierungenTile");
+				tile.setVisible(true);
+			}
+			if (this.isUserInGroup("OAV_User")) {
+				var tile = this.getView().byId("AvisierungAnmeldenTile");
+				tile.setVisible(true);
+			}
+		},
+		isUserInGroup: function(groupName) {
+			var user = this.getUser();
+			if (user.groups && user.groups.length > 0) {
+				var result = user.groups.filter(group => group.value === groupName);
+				return !!result && result.length > 0;
+			}
+			return false;	
+		},
+		getUser: function () {
+			var currentUser = this.getCurrentUser();
+			if (currentUser) {
+				var userName = currentUser.name;
+				var aData = jQuery.ajax({
+					type: "GET",
+					contentType: "application/scim+json",
+					url: "/IAS/scim/Users/" + userName,
+					dataType: "json",
+					async: false
+				});
+				if (aData.status == 200) {
+					return aData.responseJSON;
+				}
+			}
+			return undefined;
+		},
+		getCurrentUser: function () {
+			var aData = jQuery.ajax({
+				type: "GET",
+				contentType: "application/json",
+				url: "/services/userapi/currentUser",
+				dataType: "json",
+				async: false
+			});
+			if (aData.status == 200) {
+				return aData.responseJSON;
+			}
+			return undefined;
+		},
 		action: function (oEvent) {
 			var that = this;
 			var actionParameters = JSON.parse(oEvent.getSource().data("wiring").replace(/'/g, "\""));
--- webapp/controller/OAV_Add.controller.js
+++ webapp/controller/OAV_Add.controller.js
@@ -8,9 +8,6 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("test.Test.controller.main", {
-		
-	
-    
 
 	onInit: function () {
 		var oModel = new JSONModel();
@@ -76,6 +73,30 @@ sap.ui.define([
 	  	    listB.filter(aFilter);                
 		}
 
+    },
+    navBack: function(event) {
+    	var wizard = this.getView().byId("CreateAvisierungWizard");
+		var step1 = this.getView().byId("LieferdatenStep");
+		wizard.discardProgress(step1);
+    	this.getOwnerComponent().getRouter().navTo("Home");
+    },
+    wizardCompletedHandler: function(event) {
+    	// instantiate dialog
+		if (!this._dialog) {
+			this._dialog = sap.ui.xmlfragment("com.wieland.oav.view.BusyDialog", this);
+			this.getView().addDependent(this._dialog);
+		}
+
+		// open dialog
+		jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._dialog);
+		this._dialog.open();
+		var that = this;
+
+		// simulate end of operation
+		jQuery.sap.delayedCall(3000, this, function () {
+			this._dialog.close();
+			that.navBack();
+		}.bind(this));
     }
 });
 });
