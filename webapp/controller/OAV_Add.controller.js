fbf7ef28fdcaccd91afebe3b889eaeb4d6e47f28
P2000944285 <markus.schlenz@wieland.com>
2018-11-29 13:54:04 +0000

    Neue View Neues Glück

---
+++ webapp/controller/OAV_Add.controller.js
@@ -0,0 +1,81 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller,JSONModel,MessageBox,Filter,FilterOperator) {
+	"use strict";
+
+	return Controller.extend("test.Test.controller.main", {
+		
+	
+    
+
+	onInit: function () {
+		var oModel = new JSONModel();
+		oModel.setData({
+					"bloeckeCollection":[
+						{ "Text": "06:00 - 10:00",
+						   "State":"5/5",
+						   "Ladestelle": "L2",
+						   "BlockDate": "2018-11-28"
+	                    },
+	                    { "Text": "10:00 - 12:00",
+						   "State":"5/5",
+						   "Ladestelle": "L2",
+						   "BlockDate": "2018-11-28"
+	                    },
+	                    { "Text": "06:00 - 10:00",
+						   "State":"5/5",
+						   "Ladestelle": "L1",
+						   "BlockDate": "2018-11-28"
+	                    },
+	                    { "Text": "06:00 - 10:00",
+						   "State":"5/5",
+						   "Ladestelle": "L2",
+						   "BlockDate": "2018-11-29"
+	                    }
+					]
+  	    });
+  	    
+  	    this.getView().setModel(oModel,"Bloecke");
+  	    var aFilter = [];
+  	    this.byId("intervalCalendar").setNonWorkingDays([5,6]); 
+  	    var list = this.byId("ListTest");
+  	    var listB = list.getBinding("items");
+  	    aFilter.push(new Filter("BlockDate", FilterOperator.EQ, "2018-11-28"));
+  	    aFilter.push(new Filter("Ladestelle", FilterOperator.EQ, "L2"));
+  	    listB.filter(aFilter);
+  	},
+  	onFahrzeugtypChange: function (event) {
+  	    var param = event.getParameters("selectedItem");
+  	    var selectedItem = param.selectedItem.getKey();
+  	    if("LAU" === selectedItem || "LHA" === selectedItem) {
+  	    	this.byId("label_Fahrzeugkennzeichen2").setVisible(true);
+  	    	this.byId("input_Fahrzeugkennzeichen2").setVisible(true);
+  	    } else{
+  	    	this.byId("label_Fahrzeugkennzeichen2").setVisible(false);
+  	    	this.byId("input_Fahrzeugkennzeichen2").setVisible(false);
+  	    }
+  	},
+	onAddPosition: function () {
+		MessageBox.show("asdfas dafdfasdf");
+	},
+    handleCalendarSelect: function(event){
+    	var d = event.getSource().getSelectedDates()[0].getStartDate();
+		if(!!d){
+			var dateStr = d.getFullYear() + "-" + (d.getMonth() +1) + "-" + 	d.getDate();
+		  
+  	        var aFilter = [];
+	  	    this.byId("intervalCalendar").setNonWorkingDays([0,1,2]); 
+	  	    var list = this.byId("ListTest");
+	  	    var listB = list.getBinding("items");
+	  	    aFilter.push(new Filter("BlockDate", FilterOperator.EQ, dateStr));
+	  	    aFilter.push(new Filter("Ladestelle", FilterOperator.EQ, "L2"));
+	  	    listB.filter(aFilter);                
+		}
+
+    }
+});
+});
